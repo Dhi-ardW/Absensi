d79794a71a95d27ddcb8a9fb1520dba2c7bf0129
Ard <78478158+RBARAB@users.noreply.github.com>
2025-09-06 14:26:56 +0700

    membuat login page dan fitur logout

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-const absensi = document.querySelectorAll('.absensi');
+const absensi = document.querySelectorAll(".absensi");
 
-absensi.forEach(item => {
-    item.addEventListener('click', function(){
-        absensi.forEach(i => i.classList.remove('active'));
-    item.classList.add('active'); 
-    })
-})
+absensi.forEach((item) => {
+  item.addEventListener("click", function () {
+    absensi.forEach((i) => i.classList.remove("active"));
+    item.classList.add("active");
+  });
+});
+
+// notification
+const notif = document.querySelector(".notif");
+const logout = document.getElementById("logout");
+const cardnotif = document.querySelector(".card-notif");
+const btnLogout = document.querySelector(".btn-alert:nth-child(1)");
+const btnKembali = document.querySelector(".btn-alert:nth-child(2)");
+
+logout.addEventListener("click", function () {
+  notif.classList.add("active");
+  cardnotif.classList.add("active");
+  btnKembali.addEventListener("click", function () {
+    notif.classList.remove("active");
+    cardnotif.classList.remove("active");
+  });
+});
+
+// login page
